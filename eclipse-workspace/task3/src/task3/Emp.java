6f4264eac8bd84aa94f58e6e16e70d0b7d679131
REddy <Reddy90@gmail.com>
2025-05-02 08:00:50 +0530

    first commit

---
+++ eclipse-workspace/task3/src/task3/Emp.java
@@ -0,0 +1,14 @@
+package task3;
+
+public class Emp {
+   String name;
+   int eid;
+   float salary;
+   static String cname="Amazon";
+   int phno;
+ 
+@Override
+public String toString() {
+	return "Emp [name=" + name + ", eid=" + eid + ", salary=" + salary + ", cname=" + cname + ", phno=" + phno + "]";
+ }
+}
