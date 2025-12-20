2ab57755d3b1d2fcb7cb0706ca5f47f36e21554c
KattaAshuReddy <kattaashwitha2004@gmail.com>
2025-12-20 17:03:59 +0530

    Add files via upload
    The Spring Placement Management System is a web-based application developed using Spring Boot to manage and streamline college placement activities. The system helps administrators manage colleges, students, and placement-related data efficiently through RESTful services. It follows a layered architecture to ensure clean code structure, scalability, and maintainability.
    The application implements CRUD operations using Spring Data JPA and connects to a relational database for persistent storage. It exposes REST APIs that allow seamless data management and can be easily extended with additional modules such as student registration, company management, and placement tracking.

---
+++ TNSPlacementModule/springplacementproject/src/main/java/com/example/springplacementproject/SpringplacementprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springplacementproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringplacementprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringplacementprojectApplication.class, args);
+	}
+
+}
